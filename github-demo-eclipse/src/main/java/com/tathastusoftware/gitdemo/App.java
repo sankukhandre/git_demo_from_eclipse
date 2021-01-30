d807db863b8b73874b036a2fea890f0e6a02d182
sankukh <sankumisme@gmail.com>
2021-01-30 23:33:10 +0530

    First commit

---
+++ github-demo-eclipse/src/main/java/com/tathastusoftware/gitdemo/App.java
@@ -0,0 +1,14 @@
+package com.tathastusoftware.gitdemo;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void display() {
+    	System.out.println("In display");
+    }
+}
